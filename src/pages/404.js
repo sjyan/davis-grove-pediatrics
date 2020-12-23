48dee587865ee7a9cbdc3b33da050e957f5df7c8
Stephen Yan <sjyan@outlook.com>
2020-12-23 11:16:59 -0500

    Properly style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,17 @@ import Layout from '../components/Layout';
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <Layout>
-        <h1>Not Found</h1>
-        <p>You just hit a page that doesn&#39;t exist...</p>
+      <Layout bodyClass="secondary">
+        <div className="intro strip strip-grey">
+          <div className="container">
+            <div className="row justify-content-start">
+              <div className="col-12 col-md-7 col-lg-6 order-2 order-md-1">
+                <h1>Sorry!</h1>
+                <p>We couldn&#39;t find what you were looking for...</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     );
   }
